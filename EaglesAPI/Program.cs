686f54b56dfebd3a8037be1170497ccd6548466b
avinashc <avinashc@udel.edu>
2024-04-22 21:51:24 -0400

    initial

--- EaglesAPI/Program.cs
+++ EaglesAPI/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("EaglesOracleConnection");
+builder.Services.AddDbContext<EaglesOracleContext>
+(options => options.UseOracle(connectionString)
+.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
